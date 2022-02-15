ff181ee496e181e443e212b830b7e4968e518ad5
Dmitry Grigorev <helworldd@yandex.ru>
2022-02-15 17:27:44 +0300

    Test

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,62 +47,116 @@ $(document).ready(function() {
     });
 });
 
+console.log('Init!');
+
 // inputmask
-const form = $('.request__form');
-const tel = $('.tel')
+const form = document.querySelector('.form');
+const telSelector = form.querySelector('input[type="tel"]');
 const inputMask = new Inputmask('+7 (999) 999-99-99');
-inputMask.mask(tel)
+inputMask.mask(telSelector);
 
-new window.JustValidate('.request__form', {
-    rules: {
-        name: {
-            required: true,
-            minLength: 6,
-        },
-        company: {
-            required: true,
-            minLength: 6,
-        },
-        tel: {
-            required: true,
-            function: () => {
-                const phone = tel.inputmask.unmaskedvalue();
-                return Number(phone) && phone.length === 10;
-            }
-        },
-    },
-    messages: {
-        name: {
-            required: 'Введите своё имя',
-            minLength: 'Введите не меньше 6 символов',
-        },
-        company: {
-            required: 'Введите имя компании',
-            minLength: 'Введите не меньше 6 символов',
-        },
-        email: {
-            required: 'Введите почту',
-        },
-        tel: {
-            required: 'Введите свой номер телефона',
-            function: 'Напишите 10 символов телефона',
-        },
-    },
-    submitHandler: function(thisForm) {
-        let formData = new FormData(thisForm);
-
-        let xhr = new XMLHttpRequest();
-
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    console.log('Отправлено');
-                }
-            }
-        }
-        xhr.open('POST', 'mail.php', true);
-        xhr.send(formData);
-
-        thisForm.reset();
+new window.JustValidate('.form', {
+  rules: {
+    tel: {
+      required: true,
+      function: () => {
+        const phone = telSelector.inputmask.unmaskedvalue();
+        return Number(phone) && phone.length === 10;
+      }
     }
+  },
+  colorWrong: '#ff0f0f',
+  messages: {
+    name: {
+      required: 'Введите имя',
+      minLength: 'Введите 3 и более символов',
+      maxLength: 'Запрещено вводить более 15 символов'
+    },
+    email: {
+      email: 'Введите корректный email',
+      required: 'Введите email'
+    },
+    tel: {
+      required: 'Введите телефон',
+      function: 'Здесь должно быть 10 символов без +7'
+    }
+  },
+  submitHandler: function(thisForm) {
+    let formData = new FormData(thisForm);
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          console.log('Отправлено');
+        }
+      }
+    }
+
+    xhr.open('POST', 'mail.php', true);
+    xhr.send(formData);
+
+    thisForm.reset();
+  }
 })
+
+// inputmask
+// const form = $('.request__form');
+// const tel = $('.tel')
+// const inputMask = new Inputmask('+7 (999) 999-99-99');
+// inputMask.mask(tel)
+
+// new window.JustValidate('.request__form', {
+//     rules: {
+//         name: {
+//             required: true,
+//             minLength: 6,
+//         },
+//         company: {
+//             required: true,
+//             minLength: 6,
+//         },
+//         tel: {
+//             required: true,
+//             function: () => {
+//                 const phone = tel.inputmask.unmaskedvalue();
+//                 return Number(phone) && phone.length === 10;
+//             }
+//         },
+//     },
+//     messages: {
+//         name: {
+//             required: 'Введите своё имя',
+//             minLength: 'Введите не меньше 6 символов',
+//         },
+//         company: {
+//             required: 'Введите имя компании',
+//             minLength: 'Введите не меньше 6 символов',
+//         },
+//         email: {
+//             required: 'Введите почту',
+//         },
+//         tel: {
+//             required: 'Введите свой номер телефона',
+//             function: 'Напишите 10 символов телефона',
+//         },
+//     },
+//     submitHandler: function(thisForm) {
+//         let formData = new FormData(thisForm);
+
+//         let xhr = new XMLHttpRequest();
+
+//         xhr.onreadystatechange = function() {
+//             if (xhr.readyState === 4) {
+//                 if (xhr.status === 200) {
+//                     console.log('Отправлено');
+//                 }
+//             }
+//         }
+//         xhr.open('POST', 'mail.php', true);
+//         xhr.send(formData);
+
+//         thisForm.reset();
+//     }
+// })
